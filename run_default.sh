5493dfc877dcf95484f58d52cc00ca544bf3bd06
HongtuZ <zhouhongtu.cn@gmail.com>
2026-07-21 10:40:12 +0000

    update

--- run_default.sh
+++ run_default.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMON_FLAGS="--agent=agents/d3fql.py --agent.num_ensembles=5 --agent.bc_candidates=5 --agent.critic_lcb_kappa=1.0 --agent.lcb_kappa=1.0 --agent.qw_temperature=1.0 --agent.beta=0.9 --gpu_ids=0,1,2,3,4,5,6,7 --seeds=0,1,2,3,4,5,6,7"
+COMMON_FLAGS="--agent=agents/d3fql.py --agent.num_ensembles=5 --agent.bc_candidates=10 --agent.critic_lcb_kappa=0.5 --agent.lcb_kappa=0.5 --agent.qw_temperature=0.001 --agent.beta=1.0 --gpu_ids=0,1,2,3,4,5,6,7 --seeds=0,1,2,3,4,5,6,7"
 
 uv run main.py --env_name=antmaze-large-navigate-singletask-v0 ${COMMON_FLAGS} --agent.alpha=10
 uv run main.py --env_name=antmaze-giant-navigate-singletask-v0 ${COMMON_FLAGS} --agent.discount=0.995 --agent.alpha=10
